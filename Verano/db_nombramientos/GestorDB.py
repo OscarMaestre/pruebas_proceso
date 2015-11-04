0eaf49fe3733b555e0e527a6dd36733f593ee082
Pepe Perez <prueba@gmail.com>
2015-11-04 18:21:06 +0100

    Arreglado GestorDB

--- Verano/db_nombramientos/GestorDB.py
+++ Verano/db_nombramientos/GestorDB.py
@@ -26,7 +26,53 @@ if (platform.system()=="Windows"):
 
 
 
+def get_preludio_sql(nombre_funcion):
+	preludio_sql="Public Function "+nombre_funcion+"()"
+	
+	preludio_sql+="""
 
+On Error Resume Next
+
+  Dim ws As Workspace
+  Dim db As Database
+  Dim strSQL As String
+
+  Set ws = DBEngine.Workspaces(0)
+  Set db = ws.Databases(0)
+
+On Error GoTo Proc_Err
+  'start a transaction to ensure all updates are run or rolled back
+  ws.BeginTrans
+"""
+	return preludio_sql
+
+def crear_sentencia_update(sentencia):
+	sql="\t"+sentencia+"\n"
+	sql+="\tdb.Execute strSQL, dbFailOnError\n"
+	return sql
+	
+	
+def get_procedimiento(nombre, sql_intermedio):
+	inicio=get_preludio_sql(nombre)
+	fin=get_fin_sql()
+	return inicio+sql_intermedio+fin
+def get_fin_sql():
+	sql="""
+		 'commit all changes 
+  ws.CommitTrans
+
+Proc_Exit:
+  Set ws = Nothing
+  Set db = Nothing
+  Exit Function
+
+Proc_Err:
+  ws.Rollback
+  MsgBox "Error updating: " & Err.Description
+  Resume Proc_Exit
+End Function
+	"""
+	return sql
 
 class GestorDB(object):
     
