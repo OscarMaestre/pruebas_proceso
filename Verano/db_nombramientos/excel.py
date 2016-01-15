4c28e0c5760072fada3683be39da6ac10b66494f
oscar <oscar@example.com>
2016-01-15 23:54:15 +0100

    Añadido soporte Excel

--- Verano/db_nombramientos/excel.py
+++ Verano/db_nombramientos/excel.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import xlsxwriter
+
 class GeneradorExcel(object):
-    def __init__(self):
-        pass
+    def __init__(self, nombre_archivo):
+        self.nombre_archivo=nombre_archivo
+        self.libro=xlsxwriter.Workbook( self.nombre_archivo )
+        self.hoja=self.libro.add_worksheet()
+        
+    def insertar(self, fila, columna, valor):
+        self.hoja.write ( fila, columna, valor )
+    
+    def cerrar(self):
+        self.libro.close()
+        
