0c67c905939346eba93aa5f9d52e2433289876f8
Pepe Perez <prueba@gmail.com>
2016-02-10 12:00:28 +0100

    Admin especialidades

--- web/gestionweb/admin.py
+++ web/gestionweb/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Localidades
+from .models import Localidades, Especialidades
 
 admin.site.register(Localidades)
