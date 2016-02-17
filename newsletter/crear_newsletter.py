a260205b32b41e91b76bd817dbc120988ce0e014
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-02-17 16:49:37 +0100

    News

--- newsletter/crear_newsletter.py
+++ newsletter/crear_newsletter.py
@@ -10,13 +10,16 @@ sys.path.insert(0, DIRECTORIO_UTILIDADES)
 
 from utilidades.basedatos.GestorMySQL import GestorMySQL
 from utilidades.fechas.GestorFechas import GestorFechas
+from utilidades.ficheros.GestorFicheros import GestorFicheros
 
 TABLA_NEWSLETTER    =   "jos_acymailing_list"
 TABLA_HISTORIAS     =   "jos_content"
 TIPO_NEWSLETTER     =   "news"
 MOSTRAR_IMAGEN      =   1
 NO_MOSTRAR_IMAGEN   =   0
-
+NUM_SUBDIRECTORIOS_ANTERIORES = 3
+FICHERO_CONFIGURACION=(NUM_SUBDIRECTORIOS_ANTERIORES * (".." + os.sep ) ) + "configuracion_mysql.txt"
+LINEAS_FICHERO_CONFIGURACION=GestorFicheros().get_lineas_fichero ( FICHERO_CONFIGURACION )
 ASUNTO = "Boletín informativo de {0}"
 PLANTILLA_NOTICIA=" {joomlacontent:{0}| type:intro| link| pict:{1}| format:TOP_LEFT}<br/>"
 
@@ -24,8 +27,6 @@ PLANTILLA_NOTICIA=" {joomlacontent:{0}| type:intro| link| pict:{1}| format:TOP_L
 gestor_fechas=GestorFechas()
 (hace_una_semana, hoy)=gestor_fechas.get_rango_fechas_ultima_semana()
 print (hace_una_semana)
-gestor_mysql=GestorMySQL("root", "1234", "localhost", "bd_joomla", debug=True)
-cursor=gestor_mysql.ejecutar_select("select * from {0}".format ( TABLA_NEWSLETTER ))
 
-gestor_mysql=GestorMySQL("root", "1234", "localhost", "borrar", debug=True)
-cursor=gestor_mysql.ejecutar_select("insert into tabla(id, num) values (%s, %s)", (1,1), ejecutar_commit=True);
+fecha_hace_una_semana=str(hace_una_semana)[:10]
+print (fecha_hace_una_semana+"-")
