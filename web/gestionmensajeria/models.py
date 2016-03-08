3945a558cb38e5fe36a28f7447aff07e5e3d9e96
oscar <oscar@example.com>
2016-03-08 09:21:59 +0100

    Django modelos

--- web/gestionmensajeria/models.py
+++ web/gestionmensajeria/models.py
@@ -3,29 +3,19 @@ from django.db import models
 from gestionweb.models import Gaseosa
 # Create your models here.
 
-class GrupoMensajeria(models.Model):
-    nombre_grupo=models.CharField(max_length=100)
-    
-    
+
 class SistemaMensajeria(models.Model):
     nombre_sistema=models.CharField("Sistema", max_length=50)
+
+class GrupoMensajeria(models.Model):
+    nombre_grupo=models.CharField(max_length=100)
+    sistema=models.ForeignKey(SistemaMensajeria)
     
-class SuscripcionSistemaMensajeria(models.Model):
-    movil_usuario=models.CharField("Tlf_Movil", max_length=15)
-    sistema_mensajeria=models.ForeignKey ( SistemaMensajeria )
+class AfiliadosAutorizados(models.Model):
+    dni=models.ForeignKey(Gaseosa)
+    
+    
 
 class Mensaje(models.Model):
     texto=models.CharField(max_length=4096)
     sistema_a_usar=models.ForeignKey ( SistemaMensajeria )
-    
-class MensajePendiente(models.Model):
-    mensaje=models.ForeignKey ( Mensaje)
-    movil_usuario=models.CharField("Tlf_Movil", max_length=15)
-
-class MensajeBienEnviado(models.Model):
-    mensaje=models.ForeignKey ( Mensaje )
-    movil_usuario=models.CharField("Tlf_Movil", max_length=15)
-    
-class MensajeMalEnviado(models.Model):
-    mensaje=models.ForeignKey ( Mensaje )
-    movil_usuario=models.CharField("Tlf_Movil", max_length=15)
--- web/gestionweb/admin.py
+++ web/gestionweb/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Localidades, Especialidades, Correspondencias
-
+from gestionmensajeria.models import GrupoMensajeria, SistemaMensajeria
 admin.site.register(Localidades)
 admin.site.register(Especialidades)
 admin.site.register(Correspondencias)
+admin.site.register(GrupoMensajeria)
+admin.site.register(SistemaMensajeria)
